ef01a795c5cfc9c95ad48271b67eb9266e25be85
chris <chr33s@icloud.com>
2020-07-27 19:20:45 +0700

    update: read (onclick) & link (ondblclick)

--- docs/index.js
+++ docs/index.js
@@ -4,21 +4,42 @@ const createElement = (type, text = '') => {
   return el;
 };
 
-
 (async () => {
   const res = await fetch('data/cache.json')
   const data = await res.json();
 
+  const read = JSON.parse(window.localStorage.getItem('read')) || [];
+
   const ul = document.querySelector('main ul');
   data.forEach((v) => {
     const li = createElement('li');
+    const a = createElement('a');
     const h2 = createElement('h2', v.title);
     const span = createElement('span', v.date);
     const p = createElement('p', v.content);
 
-    li.appendChild(h2);
-    li.appendChild(span);
-    li.appendChild(p);
+    a.appendChild(h2);
+    a.appendChild(span);
+    a.appendChild(p);
+    if (read.includes(v.url)) {
+      a.classList.add('read');
+    }
+    const onclick = (e) => {
+      e.preventDefault();
+
+      read.push(v.url);
+      window.localStorage.setItem('read', JSON.stringify(read));
+      a.classList.add('read');
+    }
+
+    a.onclick = onclick;
+    a.ondblclick = (e) => {
+      onclick(e);
+
+      window.open(v.url);
+    }
+
+    li.appendChild(a);
 
     ul.appendChild(li);
   })
